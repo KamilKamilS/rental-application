c640af809ae90aac1bc7a36a94fc4d1cfbcbe733
Kamil <s.kamilx@gmail.com>
2021-12-27 17:36:42 +0100

    Add new Apartment - move creation of Apartment from ApartmentApplicationService to ApartmentFactory

---
+++ src/main/java/com/kamil/rentalapplication/domain/apartment/ApartmentFactory.java
@@ -0,0 +1,18 @@
+package com.kamil.rentalapplication.domain.apartment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ApartmentFactory {
+    public Apartment create(String ownerId, String street, String postalCode, String houseNumber, String apartmentNumber,
+                            String city, String country, String description, Map<String, Double> roomsDefinition) {
+        Address adress = new Address(street, postalCode, houseNumber, apartmentNumber, city, country);
+        List<Room> rooms = new ArrayList<>();
+        roomsDefinition.forEach((name, size) ->
+            rooms.add(new Room(name, new SquareMeter(size)))
+        );
+
+        return new Apartment(ownerId, adress, description);
+    }
+}
